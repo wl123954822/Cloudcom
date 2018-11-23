2a1f634f7ce480775e45569df2904db7d7c18938
wanglei <819877130@qq.com>
2018-11-23 17:50:24 +0800

    修改配置。

---
+++ service-getway/src/main/java/com/wl/servicegetway/ServiceGetwayApplication.java
@@ -0,0 +1,12 @@
+package com.wl.servicegetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGetwayApplication.class, args);
+    }
+}
